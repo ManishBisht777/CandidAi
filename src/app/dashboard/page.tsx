23c3deec814aa6acec5435e2db7df1753c7de36b
ManishBisht7 <manishbisht9711@gmail.com>
2024-08-10 21:26:48 +0530

    feat: themes and project setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <div>
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorum
+      architecto quaerat illo maiores neque commodi totam quas officia nostrum
+      non a ab, beatae id, repudiandae et similique. Veniam, sunt?
+    </div>
+  );
+}
